47fad764bdb1da2cb1e548026965b01a6d087127
huangs <thuhuangs09@gmail.com>
2014-01-11 15:23:25 +0800

    return SQL Server objects not JDBC objects

--- R/class.R
+++ R/class.R
@@ -11,7 +11,20 @@ SQLServer <- function(identifier.quote=NA) {
 }
 
 setMethod("dbConnect", "SQLServerDriver", def=function(drv, url, user='', password='', ...) {
-  drv <- callNextMethod(drv, url, user, password, ...)
+  jc <- .jcall("java/sql/DriverManager","Ljava/sql/Connection;","getConnection", as.character(url)[1], as.character(user)[1], as.character(password)[1], check=FALSE)
+  if (is.jnull(jc) && !is.jnull(drv@jdrv)) {
+    # ok one reason for this to fail is its interaction with rJava's
+    # class loader. In that case we try to load the driver directly.
+    oex <- .jgetEx(TRUE)
+    p <- .jnew("java/util/Properties")
+    if (length(user)==1 && nchar(user)) .jcall(p,"Ljava/lang/Object;","setProperty","user",user)
+    if (length(password)==1 && nchar(password)) .jcall(p,"Ljava/lang/Object;","setProperty","password",password)
+    l <- list(...)
+    if (length(names(l))) for (n in names(l)) .jcall(p, "Ljava/lang/Object;", "setProperty", n, as.character(l[[n]]))
+    jc <- .jcall(drv@jdrv, "Ljava/sql/Connection;", "connect", as.character(url)[1], p)
+  }
+  .verify.JDBC.result(jc, "Unable to connect SQL Server to ",url)
+  new("SQLServerConnection", jc=jc, identifier.quote=drv@identifier.quote)
 }, valueClass = "SQLServerConnection")
 
 ### SQLServerConnection
@@ -19,8 +32,30 @@ setMethod("dbConnect", "SQLServerDriver", def=function(drv, url, user='', passwo
 setClass("SQLServerConnection", representation("JDBCConnection", jc="jobjRef", identifier.quote="character"))
 
 setMethod("dbSendQuery", signature(conn="SQLServerConnection", statement="character"),  def=function(conn, statement, ..., list=NULL) {
-  Result <- getMethod("dbSendQuery", signature(conn="RDBCConnection", statement="character"))(conn, statment, ..., list=list)
-  
+  statement <- as.character(statement)[1L]
+  ## if the statement starts with {call or {?= call then we use CallableStatement 
+  if (isTRUE(as.logical(grepl("^\\{(call|\\?= *call)", statement)))) {
+    s <- .jcall(conn@jc, "Ljava/sql/CallableStatement;", "prepareCall", statement, check=FALSE)
+    .verify.JDBC.result(s, "Unable to execute JDBC callable statement ",statement)
+    if (length(list(...))) .fillStatementParameters(s, list(...))
+    if (!is.null(list)) .fillStatementParameters(s, list)
+    r <- .jcall(s, "Ljava/sql/ResultSet;", "executeQuery", check=FALSE)
+    .verify.JDBC.result(r, "Unable to retrieve JDBC result set for ",statement)
+  } else if (length(list(...)) || length(list)) { ## use prepared statements if there are additional arguments
+    s <- .jcall(conn@jc, "Ljava/sql/PreparedStatement;", "prepareStatement", statement, check=FALSE)
+    .verify.JDBC.result(s, "Unable to execute JDBC prepared statement ", statement)
+    if (length(list(...))) .fillStatementParameters(s, list(...))
+    if (!is.null(list)) .fillStatementParameters(s, list)
+    r <- .jcall(s, "Ljava/sql/ResultSet;", "executeQuery", check=FALSE)
+    .verify.JDBC.result(r, "Unable to retrieve JDBC result set for ",statement)
+  } else { ## otherwise use a simple statement some DBs fail with the above)
+    s <- .jcall(conn@jc, "Ljava/sql/Statement;", "createStatement")
+    .verify.JDBC.result(s, "Unable to create simple JDBC statement ",statement)
+    r <- .jcall(s, "Ljava/sql/ResultSet;", "executeQuery", as.character(statement)[1], check=FALSE)
+    .verify.JDBC.result(r, "Unable to retrieve JDBC result set for ",statement)
+  } 
+  md <- .jcall(r, "Ljava/sql/ResultSetMetaData;", "getMetaData", check=FALSE)
+  .verify.JDBC.result(md, "Unable to retrieve JDBC result set meta data for ",statement, " in dbSendQuery")
   new("SQLServerResult", jr=r, md=md, stat=s, pull=.jnull())
 })
 
@@ -31,6 +66,16 @@ setMethod("dbSendQuery", signature(conn="SQLServerConnection", statement="charac
 
 setClass("SQLServerResult", representation("JDBCResult", jr="jobjRef", md="jobjRef", stat="jobjRef", pull="jobjRef"))
 
-setMethod("dbHasCompleted", "JDBCResult", def = function(res, ...) {
+setMethod("dbHasCompleted", "SQLServerResult", def = function(res, ...) {
   TRUE
 }, valueClass = "logical")
+
+.verify.JDBC.result <- function (result, ...) {
+  if (is.jnull(result)) {
+    x <- .jgetEx(TRUE)
+    if (is.jnull(x))
+      stop(...)
+    else
+      stop(...," (",.jcall(x, "S", "getMessage"),")")
+  }
+}
